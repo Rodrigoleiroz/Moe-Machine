02cd14b8e029f952906b6b33f55ec8915b64aeb8
Rodrigoleiroz <76019058+Rodrigoleiroz@users.noreply.github.com>
2021-04-11 18:46:47 -0300

    Api+exercicios

--- Udemy/Projeto_Pokemon/api.js
+++ Udemy/Projeto_Pokemon/api.js
@@ -1,9 +1,30 @@
-const request = require("request");
-const hostname = "http://pokeapi.co/api/v2";
-const path = "/pokemon/1/";
-callback = (err, res, body) => {
-    console.log(body);
+//const request = require("request");
+//const hostname = "http://pokeapi.co/api/v2";
+//const path = "/pokemon/1/";
+//callback = (err, res, body) => {
+  //  console.log(body);
+    //}
+//request(`${hostname}${path}`, callback );
+
+
+var x = 9;
+var y = 20;
+
+if (x == 10 || y == 20){
+
+    var z = 30;
+
+    if ( z > 2) {
+
+        console.log(z);
+    
+            for ( i = 0; i - 5; i = i + 1) {
+                console.log(i);
+                
+                if ( i == 2) {
+                    console.log("i e = 2" );
+                }
+            }
+
     }
-request(`${hostname}${path}`, callback );
-
-
+}
